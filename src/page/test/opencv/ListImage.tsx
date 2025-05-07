48dd1d85676656c92be3b3afebe862827cacd762
VU LE BAO LONG <87232600+vulebaolong@users.noreply.github.com>
2025-05-07 09:11:03 +0700

    1

--- src/page/test/opencv/ListImage.tsx
+++ src/page/test/opencv/ListImage.tsx
@@ -1,3 +1,4 @@
+import { NEXT_PUBLIC_BASE_DOMAIN_API } from "@/constant/app.constant";
 import { listenToEvent } from "@/helpers/chat.helper";
 import { useSocket } from "@/hooks/socket.hook";
 import { Image, Stack } from "@mantine/core";
@@ -11,18 +12,17 @@ export default function ListImage() {
   useEffect(() => {
       if (socket) {
          listenToEvent(socket, `plate_saved`, (data) => {
-            console.log({ data });
+            console.log(`plate_saved`,{ data });
             setListImagePath((prev) => [...prev, data.image_path]);
          });
       }
    }, [socket]);
 
-   console.log({ listImagePath });
    return (
       <div>
          <Stack>
             {listImagePath.map((item, i) => {
-               return <Image key={i} src={`http://localhost:8000/${item}`} alt="Live Stream" style={{ width: "100%", maxHeight: "500px" }} />;
+               return <Image key={i} src={`${NEXT_PUBLIC_BASE_DOMAIN_API}${item}`} alt="Live Stream" style={{ width: "100%", maxHeight: "500px" }} />;
             })}
          </Stack>
       </div>
--- src/page/test/opencv/Stream.tsx
+++ src/page/test/opencv/Stream.tsx
@@ -1,3 +1,4 @@
+import { NEXT_PUBLIC_BASE_DOMAIN_API } from "@/constant/app.constant";
 import { useSocket } from "@/hooks/socket.hook";
 import { Box, Group, Image } from "@mantine/core";
 
@@ -8,7 +9,7 @@ export default function Stream() {
       <Box>
          {isConnected ? (
             <Group>
-               {/* <Image src="http://localhost:8000/video" alt="Live Stream" style={{ width: "100%", maxHeight: "500px" }} /> */}
+               <Image src={`${NEXT_PUBLIC_BASE_DOMAIN_API}stream`} alt="Live Stream" style={{ width: "100%", maxHeight: "500px" }} />
             </Group>
          ) : (
             <Box>⏳ Đang kết nối đến máy chủ...</Box>
